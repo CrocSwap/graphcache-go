40fafd9c778e9b1387fc23ad9d2e045139ffae56
Douglas R. Colkitt <douglas.colkitt@gmail.com>
2023-05-31 23:01:12 -0400

    Query and asynchronosly write liquidity

--- controller/liqRefresh.go
+++ controller/liqRefresh.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"sync"
 	"time"
 
@@ -20,17 +21,34 @@ type PositionRefresher struct {
 type LiquidityRefresher struct {
 	posQueries chan *PositionRefresher
 	query      *loader.CrocQuery
+	workers    []chan *PositionRefresher
+	nextWorker int
 }
 
+const NUM_PARALLEL_QUERIES = 200
 const QUERY_CHANNEL_WINDOW = 25000
 const POSITION_CHANNEL_WINDOW = 1000
+const QUERY_WORKER_QUEUE = 1000
 
 func NewLiquidityRefresher(query *loader.CrocQuery) *LiquidityRefresher {
+	workers := make([]chan *PositionRefresher, NUM_PARALLEL_QUERIES)
+
+	for idx := 0; idx < NUM_PARALLEL_QUERIES; idx += 1 {
+		workers[idx] = make(chan *PositionRefresher, QUERY_WORKER_QUEUE)
+	}
+
 	refresher := LiquidityRefresher{
 		posQueries: make(chan *PositionRefresher, QUERY_CHANNEL_WINDOW),
 		query:      query,
+		workers:    workers,
+		nextWorker: 0,
 	}
+
 	go refresher.watchPending()
+	for _, worker := range refresher.workers {
+		refresher.watchWorker(worker)
+	}
+
 	return &refresher
 }
 
@@ -69,8 +87,21 @@ func (r *PositionRefresher) watchPending() {
 func (r *LiquidityRefresher) watchPending() {
 	for true {
 		posRefresher := <-r.posQueries
+		r.workers[r.nextWorker] <- posRefresher
+
+		r.nextWorker = r.nextWorker + 1
+		if r.nextWorker == len(r.workers) {
+			r.nextWorker = 0
+		}
+	}
+}
+
+func (r *LiquidityRefresher) watchWorker(workQueue chan *PositionRefresher) {
+	for true {
+		posRefresher := <-workQueue
 		posType := types.PositionTypeForLiq(posRefresher.location.LiquidityLocation)
 
+		fmt.Println("Update position liquidity", posRefresher.location)
 		if posType == "ambient" {
 			ambientSeeds, err := r.query.QueryAmbientSeeds(posRefresher.location)
 			if err == nil {
