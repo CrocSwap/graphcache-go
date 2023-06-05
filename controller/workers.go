dc55b78d97de35c5520f916d33502ae4942810f0
Douglas R. Colkitt <douglas.colkitt@gmail.com>
2023-06-04 20:46:20 -0400

    Add mutex lock to Refresh handle map to prevent concurrent map write

--- controller/workers.go
+++ controller/workers.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"sync"
+
 	"github.com/CrocSwap/graphcache-go/loader"
 	"github.com/CrocSwap/graphcache-go/model"
 	"github.com/CrocSwap/graphcache-go/tables"
@@ -27,6 +29,7 @@ type RefreshAccumulator struct {
 	posRefreshers    map[types.PositionLocation]*HandleRefresher
 	koLiveRefreshers map[types.PositionLocation]*HandleRefresher
 	koPostRefreshers map[types.KOClaimLocation]*HandleRefresher
+	lock             sync.Mutex
 }
 
 func watchUpdateSeq(liq *LiquidityRefresher) chan IMsgType {
@@ -56,47 +59,59 @@ type IMsgType interface {
 func (msg *posUpdateMsg) processUpdate(accum *RefreshAccumulator, liq *LiquidityRefresher) {
 	(msg.pos).UpdatePosition(msg.liq)
 
+	accum.lock.Lock()
 	refresher, ok := accum.posRefreshers[msg.loc]
 	if !ok {
 		handle := PositionRefreshHandle{location: msg.loc, pos: msg.pos}
 		refresher = NewHandleRefresher(&handle, liq.pending)
 		accum.posRefreshers[msg.loc] = refresher
 	}
+	accum.lock.Unlock()
+
 	refresher.PushRefresh(msg.liq.Time)
 }
 
 func (msg *posImpactMsg) processUpdate(accum *RefreshAccumulator, liq *LiquidityRefresher) {
+	accum.lock.Lock()
 	refresher, ok := accum.posRefreshers[msg.loc]
 	if !ok {
 		handle := PositionRefreshHandle{location: msg.loc, pos: msg.pos}
 		refresher = NewHandleRefresher(&handle, liq.pending)
 		accum.posRefreshers[msg.loc] = refresher
 	}
+	accum.lock.Unlock()
+
 	refresher.PushRefresh(msg.eventTime)
 }
 
 func (msg *koPosUpdateMsg) processUpdate(accum *RefreshAccumulator, liq *LiquidityRefresher) {
 	cands, isPossiblyLive := (msg.pos).UpdateLiqChange(msg.liq)
 
+	accum.lock.Lock()
 	refresher, ok := accum.koLiveRefreshers[msg.loc]
 	if !ok {
 		handle := KnockoutAliveHandle{location: msg.loc, pos: msg.pos}
 		refresher = NewHandleRefresher(&handle, liq.pending)
 		accum.koLiveRefreshers[msg.loc] = refresher
 	}
+	accum.lock.Unlock()
 
 	if isPossiblyLive {
 		refresher.PushRefresh(msg.liq.Time)
 	}
 
 	for _, cand := range cands {
+		accum.lock.Lock()
 		claimLoc := types.KOClaimLocation{PositionLocation: msg.loc, PivotTime: cand.PivotTime}
 		refresher, ok := accum.koPostRefreshers[claimLoc]
+
 		if !ok {
 			handle := KnockoutPostHandle{location: claimLoc, pos: msg.pos}
 			refresher = NewHandleRefresher(&handle, liq.pending)
 			accum.koPostRefreshers[claimLoc] = refresher
 		}
+		accum.lock.Unlock()
+
 		refresher.PushRefresh(msg.liq.Time)
 	}
 }
@@ -105,6 +120,7 @@ func (msg *koCrossUpdateMsg) processUpdate(accum *RefreshAccumulator, liq *Liqui
 	cands := (msg.pos).UpdateCross(msg.cross)
 
 	for _, cand := range cands {
+		accum.lock.Lock()
 		claimLoc := msg.loc.ToClaimLoc(cand.User, cand.PivotTime)
 		refresher, ok := accum.koPostRefreshers[claimLoc]
 
@@ -114,6 +130,8 @@ func (msg *koCrossUpdateMsg) processUpdate(accum *RefreshAccumulator, liq *Liqui
 			refresher = NewHandleRefresher(&handle, liq.pending)
 			accum.koPostRefreshers[claimLoc] = refresher
 		}
+		accum.lock.Unlock()
+
 		refresher.PushRefresh(msg.cross.Time)
 	}
 }
