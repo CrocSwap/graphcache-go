5e6bac64a5af37a2e1a319999cd59e07f5855c51
Douglas R. Colkitt <douglas.colkitt@gmail.com>
2023-06-05 18:09:25 -0400

    Fix unneeded koLimit refresh logic

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM golang:1.20
 
 WORKDIR /app/
-COPY ./ /app/
 
+COPY go.mod go.sum ./
+RUN go mod download 
+
+COPY ./ /app/
 RUN go build
 
 EXPOSE 8080
-
 ENTRYPOINT ["/app/graphcache-go"]
--- controller/pools.go
+++ controller/pools.go
@@ -10,7 +10,7 @@ import (
 
 const N_POSITIONS_REFRESH_ON_SWAP = 50
 
-func (c *ControllerOverNetwork) resyncPoolOnSwap(l tables.Swap) ([]posImpactMsg, []koImpactMsg) {
+func (c *ControllerOverNetwork) resyncPoolOnSwap(l tables.Swap) []posImpactMsg {
 	loc := types.PoolLocation{
 		ChainId: c.chainId,
 		PoolIdx: l.PoolIdx,
@@ -18,20 +18,13 @@ func (c *ControllerOverNetwork) resyncPoolOnSwap(l tables.Swap) ([]posImpactMsg,
 		Quote:   types.RequireEthAddr(l.Quote),
 	}
 	positions := c.ctrl.cache.RetrievePoolPositions(loc)
-	knockouts := c.ctrl.cache.RetrievePoolLimits(loc)
 
-	hotPos := c.subsetRecent(positions, positions)
+	hotPos := c.subsetRecent(positions)
 	var msgs []posImpactMsg
 	for loc, pos := range hotPos {
 		msgs = append(msgs, posImpactMsg{loc, pos, l.Time})
 	}
-
-	hotKo := c.subsetRecent(positions, knockouts)
-	var koMsgs []koImpactMsg
-	for loc, pos := range hotKo {
-		msgs = append(msgs, koImpactMsg{loc, pos, l.Time})
-	}
-	return msgs, koMsgs
+	return msgs
 }
 
 type posMap = map[types.PositionLocation]*model.PositionTracker
@@ -55,30 +48,6 @@ func (c *ControllerOverNetwork) subsetRecent(poolPositions posMap) posMap {
 	for loc, pos := range poolPositions {
 		if !pos.IsEmpty() && pos.LatestUpdateTime >= minTime {
 			ret[loc] = pos
-		}
-	}
-
-	return ret
-}
-
-func (c *ControllerOverNetwork) subsetRecentLimits(koPositions koMap) koMap {
-	var times []int
-	for _, ko := range koPositions {
-		if !ko.IsActiveEmpty() {
-			times = append(times, ko.LatestTime)
-		}
-	}
-	sort.Sort(sort.Reverse(sort.IntSlice(times)))
-
-	minTime := 0
-	if len(times) > N_POSITIONS_REFRESH_ON_SWAP {
-		minTime = times[N_POSITIONS_REFRESH_ON_SWAP]
-	}
-
-	ret := make(koMap, 0)
-	for loc, ko := range koPositions {
-		if !ko.IsActiveEmpty() && ko.LatestTime >= minTime {
-			ret[loc] = ko
 		}
 	}
 
