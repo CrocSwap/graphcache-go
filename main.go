590a74842d056be9f6e9927162d90ed19a87cfa1
Douglas R. Colkitt <douglas.colkitt@gmail.com>
2023-06-12 16:23:12 -0400

    Update mainnet subgraph query url

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/CrocSwap/graphcache-go/cache"
 	"github.com/CrocSwap/graphcache-go/controller"
 	"github.com/CrocSwap/graphcache-go/loader"
@@ -18,15 +16,12 @@ func main() {
 
 	cache := cache.New()
 	cntrl := controller.New(netCfg, cache)
-	fmt.Println("A")
 
 	goerlChainConfig, _ := netCfg["goerli"]
 	controller.NewSubgraphSyncer(cntrl, goerlChainConfig, "goerli")
 
 	mainnetChainConfig, _ := netCfg["mainnet"]
 	controller.NewSubgraphSyncer(cntrl, mainnetChainConfig, "mainnet")
-
-	fmt.Println("B")
 
 	views := views.Views{Cache: cache, OnChain: &onChain}
 	apiServer := server.APIWebServer{Views: &views}
