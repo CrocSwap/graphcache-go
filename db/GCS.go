993f7af2e6cb9d000016d591e0af3edf3f94e156
Cade Zacharias <cade@hypotenuse.ca>
2023-07-18 16:10:40 -0500

    Add shard syncing logic

---
+++ db/GCS.go
@@ -0,0 +1,123 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"cloud.google.com/go/storage"
+	"google.golang.org/api/option"
+)
+var credentialsFile = "./db/GCS_credentials.json"
+
+// FetchBucketItems fetches the items (objects) from the specified GCS bucket and returns a slice of storage.ObjectAttrs.
+func FetchBucketItems(bucketName string) ([]*storage.ObjectAttrs, error) {
+	log.Println("[Shard Syncer]: Fetching shards from GCS")
+	var ctx = context.Background()
+	var client, err = storage.NewClient(ctx, option.WithCredentialsFile(credentialsFile))
+	
+	if err != nil {
+		return nil, err
+	}
+
+	it := client.Bucket(bucketName).Objects(ctx, nil)
+	var items []*storage.ObjectAttrs
+
+	for {
+		obj, err := it.Next()
+		if err != nil {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		items = append(items, obj)
+	}
+
+	return items, nil
+}
+
+// UploadItemToBucket uploads a local file to the specified GCS bucket with the given object name.
+func UploadItemToBucket(bucketName, objectName, filePath string) error {
+	var ctx = context.Background()
+	var client, err = storage.NewClient(ctx, option.WithCredentialsFile(credentialsFile))
+	
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	wc := client.Bucket(bucketName).Object(objectName).NewWriter(ctx)
+	if _, err := wc.Write(readFile(file)); err != nil {
+		return err
+	}
+	if err := wc.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func readFile(file *os.File) []byte {
+	content, _ := ioutil.ReadAll(file)
+	return content
+}
+
+// FetchObjectData fetches the data of a single object from the specified GCS bucket.
+func FetchObjectData(bucketName, objectName string) ([]byte, error) {
+	log.Println("[Shard Syncer]: Fetching shard from GCS", objectName)
+	var ctx = context.Background()
+	var client, err = storage.NewClient(ctx, option.WithCredentialsFile(credentialsFile))
+	
+	// client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	reader, err := client.Bucket(bucketName).Object(objectName).NewReader(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	data, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func main() {
+	// Example usage
+	bucketName := "gcgo-swap-shards"
+	// objectName := "your-object-name"
+	// filePath := "_data/file.txt"
+
+	// Fetch items from the bucket
+	items, err := FetchBucketItems(bucketName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Fetched Items:")
+	for _, item := range items {
+		fmt.Println(item.Name)
+	}
+
+	// Upload an item to the bucket
+	// err = UploadItemToBucket(bucketName, objectName, filePath)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	fmt.Println("Item uploaded successfully!")
+}
--- go.mod
+++ go.mod
@@ -10,6 +10,11 @@ require (
 )
 
 require (
+	cloud.google.com/go v0.110.2 // indirect
+	cloud.google.com/go/compute v1.19.3 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/iam v1.1.0 // indirect
+	cloud.google.com/go/storage v1.31.0 // indirect
 	github.com/StackExchange/wmi v0.0.0-20180116203802-5d049714c4a6 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/bytedance/sonic v1.8.0 // indirect
@@ -23,6 +28,13 @@ require (
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/s2a-go v0.1.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.11.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/holiman/uint256 v1.2.2-0.20230321075855-87b91420868c // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
@@ -39,12 +51,21 @@ require (
 	github.com/tklauser/numcpus v0.2.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
-	golang.org/x/crypto v0.5.0 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/oauth2 v0.8.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/api v0.126.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/grpc v1.55.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
